6ff4e285890c9cc84bfaaef9008125b1b7fc8f24
Brad Engelhardt <brad.engelhardt@gmail.com>
2018-07-20 15:52:02 -0700

    updated to webpack 4, modified webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,19 @@
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const dev = process.env.NODE_ENV !== 'production';
+
+const UglifyJsPluginConfig = new UglifyJsPlugin({
+    test: /\.js($|\?)/i,
+    sourceMap: true,
+    uglifyOptions: {
+        compress: true
+    }
+});
+
+const DefinePluginConfig = new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify('production')
+});
+
 module.exports = {
     context: __dirname + "/src",
     entry: "./index.js",
@@ -18,21 +34,14 @@ module.exports = {
             {
                 test: /\.scss$/,
                 exclude: /node_modules/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader"
-                    },
-                    {
-                        loader: "sass-loader"
-                    }
-                ]
+                loader: 'style-loader!css-loader!sass-loader'
             }
         ]
     },
+    mode: dev ? 'development' : 'production',
+    plugins: dev ? [] : [ UglifyJsPluginConfig, DefinePluginConfig ],
     node: {
         __dirname: true
-    }
+    },
+    performance: { hints: false }
 };
